856bd4ded1d9657fa8516d6770b3f1446da76500
Sam Ruby <dtrinh888@gmail.com>
2015-05-07 21:21:30 -0700

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '45555bd50a4307fd319873a80f45864fde42cb5bb96a848961ebf39b2543fe0b56e5a9aa5f47379949b6a82935072e0ababa949086e71780b55e0a244a0dc7c6'
---
+++ db/seeds.rb
@@ -0,0 +1,48 @@
+Product.delete_all
+#...
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+  description:
+    %{<p>
+        Ruby is the fastest growing and most exciting dynmaic language
+        out there. If you need to get to working programs delivered fast,
+        you should ass Ruby to your toolbox.
+        </p>},
+    image_url: 'ruby.jpg',
+    price: 49.95)
+#...
+
+#...
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+  description:
+    %{<p>
+        Ruby is the fastest growing and most exciting dynmaic language
+        out there. If you need to get to working programs delivered fast,
+        you should ass Ruby to your toolbox.
+        </p>},
+    image_url: 'ruby.jpg',
+    price: 49.95)
+#...
+
+#...
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+  description:
+    %{<p>
+        Ruby is the fastest growing and most exciting dynmaic language
+        out there. If you need to get to working programs delivered fast,
+        you should ass Ruby to your toolbox.
+        </p>},
+    image_url: 'ruby.jpg',
+    price: 49.95)
+#...
+
+#...
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+  description:
+    %{<p>
+        Ruby is the fastest growing and most exciting dynmaic language
+        out there. If you need to get to working programs delivered fast,
+        you should ass Ruby to your toolbox.
+        </p>},
+    image_url: 'ruby.jpg',
+    price: 49.95)
+#...
